75192ef74812efe739d69681fb4d753508bc3a26
yuananyun <598775508@qq.com>
2017-11-16 09:07:02 +0800

    添加配置服务注册到注册中心

--- microservice-config/microservice-config-client/src/main/java/com/yay/microservice/config/client/MicroserviceConfigClientApplication.java
+++ microservice-config/microservice-config-client/src/main/java/com/yay/microservice/config/client/MicroserviceConfigClientApplication.java
@@ -1,8 +1,12 @@
 package com.yay.microservice.config.client;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanExpressionContext;
+import org.springframework.beans.factory.config.BeanExpressionResolver;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.expression.StandardBeanExpressionResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MicroserviceConfigClientApplication {
 
+
+    @Autowired
+    private ConfigurableBeanFactory  factory;
+
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceConfigClientApplication.class, args);
     }
 
-    @Value("${foo}")
-    String foo;
+//    @Value("${foo}")
+//    String foo;
 
     @RequestMapping(value = "/hi")
     public String hi() {
-        return foo;
+        BeanExpressionResolver resolver = new StandardBeanExpressionResolver();
+        String resolvedValue = factory.resolveEmbeddedValue("${foo}");
+        return String.valueOf( resolver.evaluate(resolvedValue, new BeanExpressionContext(factory, null)));
     }
 }
