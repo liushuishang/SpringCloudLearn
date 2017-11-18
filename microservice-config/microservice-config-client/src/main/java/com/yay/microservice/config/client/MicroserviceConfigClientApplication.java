56ce39a357a2ddef31d7beb6550edd5d77461ba3
yuananyun <598775508@qq.com>
2017-11-18 17:29:29 +0800

    添加消息总线

--- microservice-config/microservice-config-client/src/main/java/com/yay/microservice/config/client/MicroserviceConfigClientApplication.java
+++ microservice-config/microservice-config-client/src/main/java/com/yay/microservice/config/client/MicroserviceConfigClientApplication.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.config.BeanExpressionResolver;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.expression.StandardBeanExpressionResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class MicroserviceConfigClientApplication {
 
 
