d31940206ee2d609e2013d253c52110e540e9a3c
yuananyun <598775508@qq.com>
2017-11-18 20:01:43 +0800

    添加zpikin服务调用跟踪组件

---
+++ microservice-tracing/microservice-zipkin/src/main/java/com/yay/microservice/zipkin/MicroserviceZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.yay.microservice.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class MicroserviceZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceZipkinApplication.class, args);
+	}
+}
